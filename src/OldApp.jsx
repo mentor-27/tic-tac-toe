199289deef18bb2699b18a7aa4d05d53d8549e1c
Matadon27 <mexanoid2012@gmail.com>
2024-04-28 04:47:20 +0300

    class names small fix

--- src/OldApp.jsx
+++ src/OldApp.jsx
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 class OldAppContainer extends Component {
 	render() {
 		return (
-			<div className="app w-450 h-650 mx-auto my-100 flex flex-col justify-between items-center py-8 px-4 rounded-2xl border border-brd-col bg-gradient-to-b from-bg-top to-bg-btm">
+			<div className="flex flex-col justify-between items-center w-450 h-650 mx-auto my-100 py-8 px-4 rounded-2xl border border-brd-col bg-gradient-to-b from-bg-top to-bg-btm">
 				<OldField />
 				<button
 					className="h-20 w-10/12 text-4xl font-semibold text-btn-txt-col bg-gradient-to-b from-btn-top to-btn-bottom border-none outline-0 rounded-lg shadow-btn-shadow duration-200 cursor-pointer hover:brightness-125 active:shadow-btn-act-shd"
